d21be068083a734871ab240ab03c11d44fe1d380
Stephen <sfritsen@gmail.com>
2023-05-11 23:46:16 -0600

    File cleanup

--- postgre.sql
+++ postgre.sql
@@ -2,6 +2,7 @@ CREATE TABLE brags(
    brag_id SERIAL PRIMARY KEY,
    user_id int NOT NULL,
    user_email VARCHAR NOT NULL,
+   title VARCHAR,
    brag TEXT,
    tags VARCHAR(255)[],
    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
