6217603f6e7ee83f2f59b3b99a29f1e280aaba37
Stephen <sfritsen@gmail.com>
2023-05-20 23:34:39 -0600

    Changed PORT variable

--- index.js
+++ index.js
@@ -95,6 +95,6 @@ app.delete("/brags/:id", async (req, res) => {
 });
 
 // Added 0.0.0.0 for railway deployment
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
