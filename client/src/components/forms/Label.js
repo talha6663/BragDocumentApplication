be1861a40c5cf0ba680596ed444fe784ee49c24c
Stephen <sfritsen@gmail.com>
2023-05-21 01:04:54 -0600

    Styling update

--- client/src/components/forms/Label.js
+++ client/src/components/forms/Label.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Label(props) {
 	return (
-		<label htmlFor={props.htmlFor} className="block font-semibold text-sm text-neutral-500">
+		<label htmlFor={props.htmlFor} className="block font-semibold text-sm text-neutral-400">
 			{props.value}
 		</label>
 	);
