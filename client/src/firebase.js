7bacbc1aa2b7bc6b3a3181a57220ee782491bfa1
Talha Arfan <126181034+talha6663@users.noreply.github.com>
2024-09-19 13:32:20 +0500

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDLYy_yERfCWwQiLfVzMDvaxyY-KpDHzP4",
-	authDomain:"bragdoc-talha.firebaseapp.com",
-	projectId: "bragdoc-talha",
-	storageBucket: "bragdoc-talha.appspot.com",
-	messagingSenderId: "305462885279",
-	appId: "1:305462885279:web:3b9905bff70fdb0d5adbaa",
-	measurementId: "G-GB3M8VVFRD"
+	apiKey: "",
+	authDomain:"",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
